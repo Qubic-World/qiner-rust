63765ac54829eb812d8bd6e5db92e47a1ad3aac9
N-010 <>
2023-08-13 01:48:14 +0300

    Update random seed

--- README.md
+++ README.md
@@ -31,11 +31,6 @@ The options to run Qiner are in the `.env` file
 1. Create a `.env' next to the built Qiner
 2. Fill in the following options: `RUST_LOG`, `NUMBER_OF_THREADS`, `ID`, `SERVER_IP`, `SERVER_PORT`, `VERSION`, `RANDOM_SEED`, `SOLUTION_THRESHOLD`
 
-#### RUST_LOG
-
-Set to `INFO` to see the output in the console
-Read more at the [link](https://docs.rs/env_logger/0.10.0/env_logger/#enabling-logging)
-
 #### NUMBER_OF_THREADS
 
 Number of threads to be mined on
@@ -48,19 +43,13 @@ Qiner ID of 60 characters
 
 IP and Port to which Qiner will connect
 
-#### VERSION
-
-Qubic version
-
 ##### Example
 
 ```
-RUST_LOG=INFO
 NUMBER_OF_THREADS=8
 ID=UBAZRCVPOZTDKGCBNPGYFUPLZXDDNHSEGJRTAJKWJBHJDKHMAKVVFAKCZGRI
 SERVER_IP=176.9.28.103
 SERVER_PORT=21841
-VERSION=1.142.1
-RANDOM_SEED=1,0,233,9,136,69,43,139
-SOLUTION_THRESHOLD=22
+RANDOM_SEED=0,0,0,0,0,0,0,0
+SOLUTION_THRESHOLD=600
 ```
--- lib/src/random_seed.rs
+++ lib/src/random_seed.rs
@@ -1,38 +1,30 @@
-use crate::types::{Seed};
+use std::env;
+use crate::env_names::ENV_RANDOM_SEED;
+use crate::types::{RANDOM_SEED_SPLIT_CHAR, Seed, SeedItem};
 
 pub fn get_random_seed() -> Seed {
-    /*    let random_seed_string = env::var(ENV_RANDOM_SEED).unwrap();
-    let split = random_seed_string.split(RANDOM_SEED_SPLIT_CHAR);*/
-    /*    for (split_item, seed_item) in split.zip(random_seed.as_mut()) {
-            *seed_item = split_item.trim().parse::<SeedItem>().unwrap();
-        }
+    let mut random_seed = Seed::default();
+    let random_seed_string = env::var(ENV_RANDOM_SEED).unwrap();
+    let split = random_seed_string.split(RANDOM_SEED_SPLIT_CHAR);
+    for (split_item, seed_item) in split.zip(random_seed.as_mut()) {
+        *seed_item = split_item.trim().parse::<SeedItem>().unwrap();
+    }
+
     random_seed
-    */
-    
-    Seed::default()
 }
 
 #[test]
 fn test_randomseed() {
-    // env::set_var(ENV_RANDOM_SEED, "  126, 27, 26, 27,    26, 27, 26, 27  ");
+    env::set_var(ENV_RANDOM_SEED, "  126, 27, 26, 27,    26, 27, 26, 27  ");
     let mut random_seed: Seed = Seed::default();
-    random_seed[0] = 0;
-    random_seed[0] = 0;
-    random_seed[2] = 0;
-    random_seed[3] = 0;
-    random_seed[4] = 0;
-    random_seed[5] = 0;
-    random_seed[6] = 0;
-    random_seed[7] = 0;
-    println!("{:?}", get_random_seed());
-    /*    random_seed[0] = 126;
-        random_seed[1] = 27;
-        random_seed[2] = 26;
-        random_seed[3] = 27;
-        random_seed[4] = 26;
-        random_seed[5] = 27;
-        random_seed[6] = 26;
-        random_seed[7] = 27;
-    */
+    random_seed[0] = 126;
+    random_seed[1] = 27;
+    random_seed[2] = 26;
+    random_seed[3] = 27;
+    random_seed[4] = 26;
+    random_seed[5] = 27;
+    random_seed[6] = 26;
+    random_seed[7] = 27;
+
     assert_eq!(random_seed, get_random_seed());
 }
